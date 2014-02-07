44037521877ed9608f9bb4a876963c4530a29209
amith.nmbr@gmail.com <amith.nmbr@gmail.com>
2014-02-06 19:26:36 -0800

    intermediate commit

--- src/main/java/com/spike/tasks/JoinCustomersHiveTask.java
+++ src/main/java/com/spike/tasks/JoinCustomersHiveTask.java
@@ -4,6 +4,8 @@ package com.spike.tasks;
 import com.google.inject.Inject;
 import com.spike.logger.Log;
 import com.spike.service.HiveService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,6 +13,7 @@ import java.sql.SQLException;
 public class JoinCustomersHiveTask implements Task {
 
     private final HiveService hiveService;
+    Logger logger = LoggerFactory.getLogger(JoinCustomersHiveTask.class);
 
     @Inject
     public JoinCustomersHiveTask(HiveService hiveService) {
@@ -26,6 +29,7 @@ public class JoinCustomersHiveTask implements Task {
         ResultSet resultSet = hiveService.executeQuery("show tables");
         try {
             while (resultSet.next()) {
+                logger.info("table name is " + resultSet.getString(1));
             }
         } catch (SQLException e) {
             e.printStackTrace();
